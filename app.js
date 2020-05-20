10555a1999a88f06136ef2688bec2bd130f860e0
f14-tomcat <rezakhakpoorboraz@gmail.com>
2020-05-19 22:07:08 -0700

    and using "event argument"

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 // const incomingarray = require(`./data.js`);
 // console.log(incomingarray.thearray[0]);
-/////////
+/////////////////
 // let incoming = require(`./data2.js`);
 // console.log(incoming.outgoingTests[0]["Test results"]);
 // console.log(incoming.outgoingTests[1]["Test results"]);
-/////////
-// Using Node built-in modules
-const MyEventEmitter = require(`events`);
-const myEmitter = new MyEventEmitter();
-myEmitter.on(`a-message`,()=>{console.log(`some message`)});
-myEmitter.emit(`a-message`);
+//////////////////
+//////// Using Node built-in modules
+// const MyEventEmitter = require(`events`);
+// const myEmitter = new MyEventEmitter();
+// myEmitter.on(`a-message`,()=>{console.log(`some message`)});
+// myEmitter.emit(`a-message`);
+/////// now adding event arguments to it
+const myEE01 = require(`events`);
+const myEE = new myEE01();
+// listener:
+myEE.on(`noise`,(aargument)=>{console.log(aargument)});
+// emitter:
+myEE.emit(`noise`,{userId:574, userName: `natalia bowman`});
