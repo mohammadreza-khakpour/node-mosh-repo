d69803054a3e8e4f642f6049825257d6a5ec882a
f14-tomcat <rezakhakpoorboraz@gmail.com>
2020-05-19 23:23:17 -0700

    how to listen on a port

--- app-http-module.js
+++ app-http-module.js
@@ -2,5 +2,15 @@ const Http = require(`http`);
 // now server, by nature, is an Instance of EventEmitter class
 const server = Http.createServer();
 // server has on() and emit() as you see:
-server.on(`mmm`,()=>{console.log(`detected`)});
-server.emit(`mmm`);
+// server.on(`mmm`,()=>{console.log(`detected`)});
+// server.emit(`mmm`);
+
+// 
+server.on("connection",(socket)=>{console.log(`New connection.....`)});
+server.listen(3000);
+console.log(`listening on port 3000`);
+// after writing all these upper lines:
+// i type node app-http-module.js to start running the module
+// and then last line appears on the console
+// then i enter http://localhost:3000/ in my browser
+// and `New connection.....` appears in console
