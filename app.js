3583f133b444d455a81ae398bcd75e9406f13696
f14-tomcat <rezakhakpoorboraz@gmail.com>
2020-05-19 21:56:02 -0700

    using "events" built-in module

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 // const incomingarray = require(`./data.js`);
 // console.log(incomingarray.thearray[0]);
-// //
-let incoming = require(`./data2.js`);
-console.log(incoming.outgoingTests[0]["Test results"]);
-console.log(incoming.outgoingTests[1]["Test results"]);
-
+/////////
+// let incoming = require(`./data2.js`);
+// console.log(incoming.outgoingTests[0]["Test results"]);
+// console.log(incoming.outgoingTests[1]["Test results"]);
+/////////
+// Using Node built-in modules
+const MyEventEmitter = require(`events`);
+const myEmitter = new MyEventEmitter();
+myEmitter.on(`a-message`,()=>{console.log(`some message`)});
+myEmitter.emit(`a-message`);
